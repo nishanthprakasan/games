1361321a27637dd0bf3fd4d3e2a71199bec68cf4
Nishanth Prakasan <84633000+nishanthprakasan@users.noreply.github.com>
2024-09-30 17:29:37 +0530

    Delete Chess directory

--- Chess/Chess/loginscreen/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LoginscreenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'loginscreen'
--- Chess/loginscreen/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path('ws/play/<int:room_id>/', consumers.ChessConsumer.as_asgi()),
-] 
